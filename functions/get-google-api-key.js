d1680bb2ae88db863e7780e45b69827bf28b1e49
biy11 <bilalyousufzai28@gmail.com>
2024-07-14 00:05:36 +0100

    API variable setting

--- functions/get-google-api-key.js
+++ functions/get-google-api-key.js
@@ -1,4 +1,6 @@
 exports.handler = async () => {
+    console.log('Environment Variable GOOGLE_API_KEY:', process.env.GOOGLE_API_KEY); // Add this line for debugging
+  
     return {
       statusCode: 200,
       headers: {
