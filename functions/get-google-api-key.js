7ae592c3e06ca0843b8d2ae3f7c0a190cb4370c6
biy11 <bilalyousufzai28@gmail.com>
2024-07-13 22:42:43 +0100

    Add CORS headers and improve error handling

--- functions/get-google-api-key.js
+++ functions/get-google-api-key.js
@@ -1,6 +1,10 @@
 exports.handler = async () => {
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Allow CORS from any origin
+        'Access-Control-Allow-Headers': 'Content-Type', // Allow specific headers
+      },
       body: process.env.GOOGLE_API_KEY,
     };
   };
